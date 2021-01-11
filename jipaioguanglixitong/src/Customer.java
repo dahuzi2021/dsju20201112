7dd060cd416c5099e6758b2905b22df68d51f1af
Administrator <1363074880@qq.com>
2021-01-11 16:15:47 +0800

    第一次修改

---
+++ jipaioguanglixitong/src/Customer.java
@@ -0,0 +1,37 @@
+public class Customer {
+    private String ID;
+    private String name;
+    private String phone;
+    private String cardID;
+
+    public String getID() {
+        return ID;
+    }
+    public void setID(){
+        this.ID=ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCardID() {
+        return cardID;
+    }
+
+    public void setCardID(String cardID) {
+        this.cardID = cardID;
+    }
+}
